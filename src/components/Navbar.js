829c006d87eb31c162ab8380b6392709cca1835e
Himanshu Banoula <himanshubanoula58@gmail.com>
2024-02-09 12:13:21 +0530

    Changing navigation

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -36,25 +36,25 @@ function Navbar () {
             </div>
             <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li className='nav-item'>
-                    <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                    <Link to='/Travels' className='nav-links' onClick={closeMobileMenu}>
                         Home
                     </Link>
                 </li>
 
                 <li className='nav-item'>
-                    <Link to='/services' className='nav-links' onClick={closeMobileMenu}>
+                    <Link to='/Travels/Services' className='nav-links' onClick={closeMobileMenu}>
                         Services
                     </Link>
                 </li>
 
                 <li className='nav-item'>
-                    <Link to='/products' className='nav-links' onClick={closeMobileMenu}>
+                    <Link to='/Travels/Products' className='nav-links' onClick={closeMobileMenu}>
                         Products
                     </Link>
                 </li>
 
                 <li className='nav-item'>
-                    <Link to='/sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
+                    <Link to='/Travels/Sign-up' className='nav-links-mobile' onClick={closeMobileMenu}>
                         Sign Up
                     </Link>
                 </li>
