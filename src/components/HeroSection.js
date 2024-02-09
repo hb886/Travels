77eb3ec65a609f5614172836d517f673495d6037
Himanshu Banoula <himanshubanoula58@gmail.com>
2024-02-09 12:47:20 +0530

    Fixing video issue

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -6,7 +6,7 @@ import './HeroSection.css'
 function HeroSection () {
   return (
     <div className='hero-container'>
-        <video src='../../public/videos/video-3.mp4' autoPlay loop muted />
+        <video src='/Travels/videos/video-3.mp4' autoPlay loop muted />
         <h1>ADVENTURE AWAITS</h1>
         <p>What are you waiting for?</p>
         
